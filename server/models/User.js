99554bf9ab1eb4fc90985a5e5ada159653e1d1c9
[Jeremy Ray] <jeremydray@gmail.com>
2024-11-06 19:29:45 -0600

    Add initial Typedefs and resolvers from models

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
+const skillSchema = require('./Skill')
+
 const userSchema = new Schema({
   firstName: {
     type: String,
@@ -24,10 +26,11 @@ const userSchema = new Schema({
     required: true,
     minlength: 5
   },
+  skill: [skillSchema]
 });
 
 // set up pre-save middleware to create password
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
@@ -37,7 +40,7 @@ userSchema.pre('save', async function(next) {
 });
 
 // compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
