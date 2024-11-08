59dcf0100be43344e8a0400fb9cf494e73dda3e7
JOrullian <jedediahorullian@gmail.com>
2024-11-07 21:39:03 -0600

    added Signup page available at /signup. Also fixed a small issue with the LoginForm.

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,23 @@
+import Logo from '../assets/logo.svg';
+import SignupForm from '../components/SignupForm';
+
+export default function Signup(props) {
+    return (
+        <>
+            <div className="signup-main-container">
+                <header className="signup-header">
+                    <div className="signup-logo-container">
+                        <img className="signup-logo" src={Logo}></img>
+                    </div>
+                    <div className="signup-header-title">
+                        <h1>Create an<br/>account</h1>
+                    </div>
+                    <div className="signup-header-subtext">
+                        <p>Enter your information</p>
+                    </div>
+                </header>
+                <SignupForm />
+            </div>
+        </>
+    )
+}
