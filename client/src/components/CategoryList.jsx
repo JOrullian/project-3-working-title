e9ba9755efd83598e1a967637f5898a32b0a5a2f
JOrullian <jedediahorullian@gmail.com>
2024-11-14 08:06:23 -0600

    ok, the category list now displays on the dashboard, however, each is undefined. We also need to figure out their images.

--- client/src/components/CategoryList.jsx
+++ client/src/components/CategoryList.jsx
@@ -1,43 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import CategoryCard from "../components/CategoryCard";
 import PropTypes from "prop-types";
-import { GET_CATEGORIES } from "../utils/queries";
-import { useQuery } from "@apollo/client";
 
-function CategoryList() {
-  const { loading, data } = useQuery(GET_CATEGORIES);
-  const categories = data?.categories || [];
+function CategoryList({ categories }) {
+  const navigate = useNavigate();
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const handleCategoryClick = (categoryTitle) => {
+    const categoryName = categoryTitle.toLowerCase();
+    navigate(`/${categoryName}`);
+  };
 
-  function CategoryList({ categories }) {
-    const navigate = useNavigate();
-
-    const handleCategoryClick = (categoryTitle) => {
-      const categoryName = categoryTitle.toLowerCase();
-      navigate(`/${categoryName}`);
-    };
-
-    return (
-      <div className="category-list">
-        {categories.map((category) => (
-          <div
-            key={category.id}
-            className="category-card-link"
-            onClick={() => handleCategoryClick(category.title)}
-          >
-            <CategoryCard
-              title={category.title}
-              imgSrc={category.imgSrc}
-              onClick={() => console.log(`Clicked on ${category.title}`)}
-            />
-          </div>
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="category-list">
+      {categories.map((category) => (
+        <div
+          key={category.id}
+          className="category-card-link"
+          onClick={() => handleCategoryClick(category.title)}
+        >
+          <CategoryCard
+            title={category.title}
+            imgSrc={category.imgSrc}
+            onClick={() => console.log(`Clicked on ${category.title}`)}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 CategoryList.propTypes = {
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,41 +1,54 @@
-import Logo from '../assets/logo.svg'
-import CategoryList from '../components/CategoryList'
-import Navbar from '../components/Navbar'
-import SearchBar from '../components/SearchBar'
+import { useQuery } from "@apollo/client";
 
-export default function Dashboard() {
-    return (
-        <>
-            <div className="page-main-container">
-                <header className="page-header-container">
-                    <div className="searchbar-container">
-                        <SearchBar />
-                    </div>
-                    <div className="notification-icon-container">
-                        <img src={Logo} alt="Logo" />
-                    </div>
-                </header>
-                <div className="category-swipe-container">
-                    <CategoryList />
-                </div>
-                <div className="promotional-container">
-                    {/* Unknown promotional space.. ads? */}
-                </div>
-                <section className="for-you-skill-section">
-                    <header className="for-you-header">
-                        <div className="for-you-title-container">
-                            <h3 className="for-you-title">For You</h3>
-                        </div>
-                        <div className="view-all-container">
-                            view all <span className="view-all-arrow">&gt;</span>
-                        </div>
-                    </header>
-                    <div className="for-you-body">
-                        {/* Section for recommended skills to be displayed based on users preferences/searches */}
-                    </div>
-                </section>
+import { GET_CATEGORIES } from "../utils/queries";
+
+import Logo from "../assets/logo.svg";
+import CategoryList from "../components/CategoryList";
+import Navbar from "../components/Navbar";
+import SearchBar from "../components/SearchBar";
+
+function Dashboard() {
+  const { loading, error, data } = useQuery(GET_CATEGORIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading categories: {error.message}</p>;
+
+  const categories = data.categories;
+
+  return (
+    <>
+      <div className="page-main-container">
+        <header className="page-header-container">
+          <div className="searchbar-container">
+            <SearchBar />
+          </div>
+          <div className="notification-icon-container">
+            <img src={Logo} alt="Logo" />
+          </div>
+        </header>
+        <div className="category-swipe-container">
+          <CategoryList categories={categories}/>
+        </div>
+        <div className="promotional-container">
+          {/* Unknown promotional space.. ads? */}
+        </div>
+        <section className="for-you-skill-section">
+          <header className="for-you-header">
+            <div className="for-you-title-container">
+              <h3 className="for-you-title">For You</h3>
             </div>
-            <Navbar />
-        </>
-    );
+            <div className="view-all-container">
+              view all <span className="view-all-arrow">&gt;</span>
+            </div>
+          </header>
+          <div className="for-you-body">
+            {/* Section for recommended skills to be displayed based on users preferences/searches */}
+          </div>
+        </section>
+      </div>
+      <Navbar />
+    </>
+  );
 }
+
+export default Dashboard;
