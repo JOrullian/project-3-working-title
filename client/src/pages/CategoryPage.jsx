071bd55eae136dc3eafb219d13b3687184eae511
JOrullian <jedediahorullian@gmail.com>
2024-11-14 19:33:09 -0600

    added seed script to render-build script to try to get seed categories to show on deployed page

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -4,22 +4,34 @@ import AppNavbar from "../components/Navbar";
 import BackArrow from "../assets/back-arrow.svg";
 
 export default function CategoryPage() {
-    const { categoryName } = useParams();
-    const navigate = useNavigate();
+  const { categoryName } = useParams();
+  const navigate = useNavigate();
 
-    const handleBackArrowClick = () => {
-        navigate(`/`);
-    }
+  const handleBackArrowClick = () => {
+    navigate(`/`);
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   return (
     <>
       <div className="page-main-container">
         <header className="category-header">
           <div className="back-arrow-container">
-            <img className="profile-backarrow" src={BackArrow} onClick={() => handleBackArrowClick()}></img>
+            <img
+              className="profile-backarrow"
+              src={BackArrow}
+              onClick={() => handleBackArrowClick()}
+            ></img>
           </div>
           <div className="category-title-container">
-            <h1>{categoryName ? categoryName.replace('-', ' ') : 'Category'}</h1>
+            <h1>
+              {categoryName
+                ? capitalizeFirstLetter(categoryName.replace("-", " "))
+                : "Category"}
+            </h1>
           </div>
         </header>
         <div className="category-page-body"></div>
